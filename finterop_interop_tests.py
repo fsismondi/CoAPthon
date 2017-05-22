f364814bced7295a037913f02804d383ecda0d4b
Federico Sismondi <federicosismondi@gmail.com>
2017-05-22 17:18:46 +0200

    modifs in cli interface

--- finterop_interop_tests.py
+++ finterop_interop_tests.py
@@ -7,9 +7,9 @@ import sys
 
 
 class Finterop_Tests(plugtest.Tests):
+    server_address = (None,None)
 
     def setUp(self): # override Tests.setUp
-        self.server_address = ("bbbb::3", 5683)
         self.current_mid = random.randint(1, 1000)
         self.server_mid = random.randint(1000, 2000)
 
@@ -22,12 +22,34 @@ def main(argv):
     testcases = [i for i in dir(Finterop_Tests) if 'test_td' in i]
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--testcase", choices=testcases,
-                        help="Choose interop testcase to execute")
+    parser.add_argument(
+            "-t",
+            "--testcase",
+            choices=testcases,
+            help="Choose interop testcase to execute"
+    )
+
+    parser.add_argument(
+            "-ip",
+            "--ip",
+            default="bbbb::2",
+            help="target IP address of the server needed. Example: 'bbbb::2'"
+    )
+
+    parser.add_argument(
+            "-p",
+            "--port",
+            default= 5683,
+            help="target port of the server needed"
+    )
+
     args = parser.parse_args()
-    print 'starting %s'%args.testcase
+    print 'selected test cases: %s'%args.testcase
+    print'ip: %s' % args.ip
+    print'port: %s' % args.port
 
     suite = unittest.TestSuite()
+    Finterop_Tests.server_address = (args.ip, args.port)
     suite.addTest(Finterop_Tests(args.testcase))
     runner = unittest.TextTestRunner()
     runner.run(suite)
